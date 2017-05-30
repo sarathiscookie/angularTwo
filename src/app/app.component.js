2534cb4c38397f548525e31cce7daa85ed3020b7
sarath <sarath@cabin-holiday.com>
2017-05-30 14:52:40 +0530

    View API 1: Showing an array property with *ngFor

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,14 +9,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Tour of heroes';
+        this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+        this.myHero = this.heroes[0];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n      <h1>{{title}}</h1>\n      <h2>My favorite hero is: {{myHero}}</h2>\n      <p>Heroes:</p>\n      <ul>\n          <li *ngFor=\"let hero of heroes\"> {{hero}} </li>\n      </ul>\n  ",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+      <h1>{{title}}</h1>
+      <h2>My favorite hero is: {{myHero}}</h2>
+      <p>Heroes:</p>
+      <ul>
+          <li *ngFor="let hero of heroes"> {{hero}} </li>
+      </ul>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { title = 'Tour of heroes'; heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado']; myHero = this.heroes[0]; }
