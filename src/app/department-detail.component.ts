bf6ef7eed615a22798bd8eb860f9e1c2943dacd1
sarath <sarath@cabin-holiday.com>
2017-06-06 16:43:57 +0530

    View API 12: Documented

--- src/app/department-detail.component.ts
+++ src/app/department-detail.component.ts
@@ -15,7 +15,7 @@ export class DepartmentDetailComponent implements OnInit {
     public departmentID;
     constructor(private router: ActivatedRoute) {}
     ngOnInit(): void {
-        let id = this.router.snapshot.params['id'];
+        let id = this.router.snapshot.params['id']; // snapshot: It used to take the present url eg: http://192.168.10.10:3000/departments/5
         this.departmentID = id;
     }
 }
