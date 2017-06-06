83bc203536c27de3db5eca31c34da49303f63554
sarath <sarath@cabin-holiday.com>
2017-06-06 14:15:12 +0530

    View API 12: Routing - id passing from department listing to details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes} from '@angular/router';
 
 import { DepartmentComponent } from './department.component';
+import { DepartmentDetailComponent } from './department-detail.component';
 import { LogComponent } from './log.component';
 
 const routes: Routes = [
     { path: 'departments', component: DepartmentComponent },
     { path: 'logs', component: LogComponent },
+    { path: 'departments/:id', component: DepartmentDetailComponent },
 ];
 
 @NgModule({
---
+++ src/app/department-detail.component.ts
@@ -0,0 +1,32 @@
+/**
+ * Created by user on 06-06-2017.
+ */
+import {Component, OnInit} from '@angular/core';
+
+import {Department} from './department';
+import {DepartmentService} from './department.service';
+
+
+@Component({
+    selector: 'my-department',
+    template: `
+        <div class="col-md-6">
+            <h1>Departments Details</h1>
+            <div class="list-group col-md-6">
+                <a href="#" class="list-group-item" *ngFor="let dep of department">{{dep.description}}</a>
+            </div>
+        </div>
+    `,
+    providers: [DepartmentService]
+})
+
+export class DepartmentDetailComponent implements OnInit {
+    department: Department[];
+    constructor(private departmentService: DepartmentService) {}
+    getDepartment(): void {
+        this.departmentService.getDepartment().then(department => this.department = department);
+    }
+    ngOnInit(): void {
+        this.getDepartment();
+    }
+}
