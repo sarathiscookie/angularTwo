ae8a4e1602630a2452441d69d4e3fd2700294b1d
sarath <sarath@cabin-holiday.com>
2017-06-06 16:41:43 +0530

    View API 12: Routing - id passing through url and get that id

--- src/app/department-detail.component.ts
+++ src/app/department-detail.component.ts
@@ -1,13 +1,21 @@
 /**
  * Created by user on 06-06-2017.
  */
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; // We need to extract the url parameter using this service
 
 @Component({
     /*selector: 'my-department',*/
     template: `
-        <h3>Selected department id = </h3>
+        <h3>Selected department id = {{departmentID}}</h3>
     `
 })
 
-export class DepartmentDetailComponent {}
+export class DepartmentDetailComponent implements OnInit {
+    public departmentID;
+    constructor(private router: ActivatedRoute) {}
+    ngOnInit(): void {
+        let id = this.router.snapshot.params['id'];
+        this.departmentID = id;
+    }
+}
