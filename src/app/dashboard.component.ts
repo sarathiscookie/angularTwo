637107bf83d46df596f0fcbda635821ac713c91a
sarath <sarath@cabin-holiday.com>
2017-06-06 19:33:08 +0530

    View API 13: Routing - added added styles for dashboard and users.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -10,11 +10,13 @@ import { HeroService } from './hero.service';
 @Component({
     selector: 'my-dashboard',
     templateUrl: './dashboard.component.html',
+    styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
     heroes: Hero[];
     constructor(private heroService: HeroService) { }
     ngOnInit(): void {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        // this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5)); // limit to 5
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes); // No limit
     }
 }
