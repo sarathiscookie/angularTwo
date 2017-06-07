09a309268ffc924343ad720485934baddae49b42
sarath <sarath@cabin-holiday.com>
2017-06-07 17:29:33 +0530

    View API 16: Http - Added mock data

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -5,7 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms"); // <-- NgModel lives here
@@ -22,6 +21,9 @@ var department_detail_component_1 = require("./department-detail.component");
 var log_component_1 = require("./log.component");
 var page_not_found_component_1 = require("./page-not-found.component");
 var app_routing_module_1 = require("./app-routing.module");
+// Imports for loading & configuring the in-memory web api
+var angular_in_memory_web_api_1 = require("angular-in-memory-web-api");
+var in_memory_data_service_1 = require("./in-memory-data.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -33,6 +35,7 @@ AppModule = __decorate([
             platform_browser_1.BrowserModule,
             forms_1.FormsModule,
             http_1.HttpModule,
+            angular_in_memory_web_api_1.InMemoryWebApiModule.forRoot(in_memory_data_service_1.InMemoryDataService),
             app_routing_module_1.AppRoutingModule
         ],
         declarations: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,16 @@ import { PageNotFoundComponent } from './page-not-found.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports:      [
       BrowserModule,
       FormsModule,  // <-- import the FormsModule before binding with [(ngModel)]
       HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
       AppRoutingModule
   ],
   declarations: [
